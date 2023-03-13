253d08d80b3c0742ef2c9264270ae84ca5f3fab0
Robert Krauße <yes-man@web.de>
2023-03-13 17:57:37 +0100

    started 'Anybody Out There'; started 'Looks can be Deceiving'; accidentally updated LazyMaze

---
+++ AnybodyOutThere/AnybodyOutThere/Program.cs
@@ -0,0 +1,54 @@
+﻿using NLog;
+using System.Text.RegularExpressions;
+
+namespace AnybodyOutThere {
+    internal class Program {
+        private static Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        const int COUNT_X = 1000;
+        const int COUNT_Y = 300;
+        const int COUNT_RESULT = 12;
+        const int REQ_X_START = 979;
+        const int REQ_Y_START = 279;
+        const int REQ_X_COUNT = 20;
+        const int REQ_Y_COUNT = 20;
+        const int REQ_DELAY = 100;
+
+        static async Task Main(string[] args) {
+            UriBuilder requestBuilder = new UriBuilder("https://www.hacker.org/challenge/misc/twonumbers.php");
+            Regex numberParser = new Regex(".*?form>\\s*?(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>.*");
+            int[,,] results = new int[COUNT_X, COUNT_Y, COUNT_RESULT];
+
+            using (HttpClient client = new HttpClient()) {
+                for (int x = REQ_X_START; x <= REQ_X_START + REQ_X_COUNT; x++) {
+                    for (int y = REQ_Y_START; y <= REQ_Y_START + REQ_Y_COUNT; y++) {
+                        LOGGER.Info("Requesting x:" + x + " y:" + y);
+                        requestBuilder.Query = "go=Try&one=" + x + "&two=" + y;
+
+                        HttpResponseMessage foo = await client.GetAsync(requestBuilder.Uri);
+                        Match match = numberParser.Match(await foo.Content.ReadAsStringAsync());
+                        if (match.Success) {
+                            for (int i = 0; i < COUNT_RESULT; i++) {
+                                results[x, y, i] = int.Parse(match.Groups[i + 1].Value);
+                            }
+                        }
+                        else {
+                            LOGGER.Warn("Found something weird at {x}:{y} ... {weird}", x, y, match.Groups[0].Value);
+                        }
+                        Thread.Sleep(REQ_DELAY);
+                    }
+                }
+                LOGGER.Info("Done.");
+            }
+            for (int x = REQ_X_START; x <= REQ_X_START + REQ_X_COUNT; x++) {
+                for (int y = REQ_Y_START; y <= REQ_Y_START + REQ_Y_COUNT; y++) {
+                    string foo = "";
+                    for (int r = 0; r < COUNT_RESULT; r++) {
+                        foo += results[x, y, r] + (r < COUNT_RESULT - 1 ? "," : "");
+                    }
+                    LOGGER.Info(foo);
+                }
+            }
+        }
+    }
+}
---
+++ DeceivingLooks/DeceivingLooks/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DeceivingLooks {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+
+            int lim = 0x12345678; int pp = 256;
+            for (int i = 1; (lim - pp) / ((64 + i) + pp) != 0; i++) {
+                if (Math.Atan(i - ((i / lim) * lim)) > 1.55) // > ~50
+                    lim = (lim / 50) * 40;
+                int foo = lim; // bar
+                for (int j = 0; j < 0x123456; j++)
+                    foo ^= (j >> 3) | (j << 29); // tidy
+                pp &= foo; // in 42-0
+                if (lim - ((lim / i) * i) == 0) {
+                    Console.WriteLine("{0}", i / 2);
+                    //Console.WriteLine("{0:x2}", i / 2);
+                    //printf("%.2x", i / 2);
+                    pp = (pp * 3) / 2;
+                }
+            }
+        }
+    }
+}
