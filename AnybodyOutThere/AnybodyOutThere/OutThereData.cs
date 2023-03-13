4567d9bce1327a72ebdc6e3c6e66af280d413408
rkrausse <robert.krausse@web.de>
2023-03-13 23:18:33 +0100

    added parallel requests and serialization to AnybodyOutThere

---
+++ AnybodyOutThere/AnybodyOutThere/OutThereData.cs
@@ -0,0 +1,13 @@
+﻿namespace AnybodyOutThere {
+    internal class OutThereData {
+        public const int COUNT_RESULT = 12;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int[] Data { get; private set; }
+
+        public OutThereData() {
+            this.Data = new int[COUNT_RESULT];
+        }
+    }
+}
--- AnybodyOutThere/AnybodyOutThere/Program.cs
+++ AnybodyOutThere/AnybodyOutThere/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace AnybodyOutThere {
@@ -7,48 +8,74 @@ namespace AnybodyOutThere {
 
         const int COUNT_X = 1000;
         const int COUNT_Y = 300;
-        const int COUNT_RESULT = 12;
-        const int REQ_X_START = 979;
-        const int REQ_Y_START = 279;
-        const int REQ_X_COUNT = 20;
-        const int REQ_Y_COUNT = 20;
-        const int REQ_DELAY = 100;
+        const int REQ_X_START = 0;
+        const int REQ_Y_START = 0;
+        const int REQ_X_COUNT = COUNT_X;
+        const int REQ_Y_COUNT = COUNT_Y;
+        const int TASK_COUNT = 10;
 
-        static async Task Main(string[] args) {
-            UriBuilder requestBuilder = new UriBuilder("https://www.hacker.org/challenge/misc/twonumbers.php");
-            Regex numberParser = new Regex(".*?form>\\s*?(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>.*");
-            int[,,] results = new int[COUNT_X, COUNT_Y, COUNT_RESULT];
+        private static readonly UriBuilder requestBuilder = new UriBuilder("https://www.hacker.org/challenge/misc/twonumbers.php");
+        private static readonly Regex numberParser = new(".*?form>\\s*?(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>(\\d+)<br>.*");
+        private static readonly OutThereData[] results = new OutThereData[COUNT_X * COUNT_Y];
+        private static readonly Tuple<int, int, Task<HttpResponseMessage>>[] tasks = new Tuple<int, int, Task<HttpResponseMessage>>[TASK_COUNT];
 
-            using (HttpClient client = new HttpClient()) {
-                for (int x = REQ_X_START; x <= REQ_X_START + REQ_X_COUNT; x++) {
-                    for (int y = REQ_Y_START; y <= REQ_Y_START + REQ_Y_COUNT; y++) {
-                        LOGGER.Info("Requesting x:" + x + " y:" + y);
-                        requestBuilder.Query = "go=Try&one=" + x + "&two=" + y;
-
-                        HttpResponseMessage foo = await client.GetAsync(requestBuilder.Uri);
-                        Match match = numberParser.Match(await foo.Content.ReadAsStringAsync());
-                        if (match.Success) {
-                            for (int i = 0; i < COUNT_RESULT; i++) {
-                                results[x, y, i] = int.Parse(match.Groups[i + 1].Value);
-                            }
-                        }
-                        else {
-                            LOGGER.Warn("Found something weird at {x}:{y} ... {weird}", x, y, match.Groups[0].Value);
-                        }
-                        Thread.Sleep(REQ_DELAY);
-                    }
-                }
-                LOGGER.Info("Done.");
-            }
-            for (int x = REQ_X_START; x <= REQ_X_START + REQ_X_COUNT; x++) {
-                for (int y = REQ_Y_START; y <= REQ_Y_START + REQ_Y_COUNT; y++) {
+        static void printCsv() {
+            for (int x = REQ_X_START; x < REQ_X_START + REQ_X_COUNT; x++) {
+                for (int y = REQ_Y_START; y < REQ_Y_START + REQ_Y_COUNT; y++) {
                     string foo = "";
-                    for (int r = 0; r < COUNT_RESULT; r++) {
-                        foo += results[x, y, r] + (r < COUNT_RESULT - 1 ? "," : "");
+                    for (int r = 0; r < OutThereData.COUNT_RESULT; r++) {
+                        foo += results[y * COUNT_X + x].Data[r] + (r < OutThereData.COUNT_RESULT - 1 ? "," : "");
                     }
                     LOGGER.Info(foo);
                 }
             }
+        }
+
+        static async void getPreviousTaskResult(int index) {
+            int modIndex = index % TASK_COUNT;
+            if (tasks[modIndex] != null) {
+                HttpResponseMessage msg = tasks[modIndex].Item3.Result;
+
+                Match match = numberParser.Match(await msg.Content.ReadAsStringAsync());
+                if (match.Success) {
+                    for (int i = 0; i < OutThereData.COUNT_RESULT; i++) {
+                        results[tasks[modIndex].Item2 * COUNT_X + tasks[modIndex].Item1].Data[i] = int.Parse(match.Groups[i + 1].Value);
+                    }
+                }
+                else {
+                    LOGGER.Warn("Found something weird at {x}:{y} ... {weird}", tasks[modIndex].Item1, tasks[modIndex].Item2, match.Groups[0].Value);
+                }
+            }
+        }
+
+        static async Task Main(string[] args) {
+            for (int i = 0; i < COUNT_X * COUNT_Y; i++) {
+                results[i] = new OutThereData();
+            }
+
+            using (HttpClient client = new HttpClient()) {
+                int currentIndex = 0;
+
+                for (int x = REQ_X_START; x < REQ_X_START + REQ_X_COUNT; x++) {
+                    for (int y = REQ_Y_START; y < REQ_Y_START + REQ_Y_COUNT; y++) {
+                        currentIndex++;
+                        getPreviousTaskResult(currentIndex);
+
+                        LOGGER.Info("Requesting x:" + x + " y:" + y);
+                        requestBuilder.Query = "go=Try&one=" + x + "&two=" + y;
+                        tasks[currentIndex % 10] = new Tuple<int, int, Task<HttpResponseMessage>>(x, y, client.GetAsync(requestBuilder.Uri));
+                    }
+                }
+
+                for (int i = 0; i < TASK_COUNT; i++) {
+                    getPreviousTaskResult(i);
+                }
+                LOGGER.Info("Done.");
+            }
+
+            using FileStream fileStream = File.Create("E:\\Yes-Man\\Dropbox\\anybody.json");
+            await JsonSerializer.SerializeAsync(fileStream, results, new JsonSerializerOptions { WriteIndented = false });
+            await fileStream.DisposeAsync();
         }
     }
 }
